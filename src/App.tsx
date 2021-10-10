af9e5f9e8daa8c18d681bd2c3398ced1b9323c5a
BD-Git-Hub <bovindesouza@gmail.com>
2021-10-10 23:53:52 +0100

    'starting point for wheatherAPIWebsite

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <p>test</p>
+      
+      
+    </div>
+  );
+}
+
+export default App;
